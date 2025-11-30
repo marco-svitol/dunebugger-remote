2b04ef2be82da4a4b2b982d703d92d90bc12e92e
marco-svitol <marco.cambon@gmail.com>
2025-12-01 00:18:42 +0100

    handle WebSocket connection and authentication improvements

--- app/dunebugger_auth.py
+++ app/dunebugger_auth.py
@@ -26,12 +26,9 @@ class AuthClient:
         #   logger.error(f"Error during initialization: {e}")
 
     def _update_user_info(self):
-        try:
-            self._get_auth_token()
-            self._get_user_info()
-            logger.info(f"User {self.name} updated")
-        except Exception as e:
-            logger.error(f"Error updating user info: {e}")
+        self._get_auth_token()
+        self._get_user_info()
+        logger.info(f"User {self.name} updated")
 
     def _get_auth_token(self):
         payload = json.dumps(
@@ -44,35 +41,31 @@ class AuthClient:
                 "scope": "openid profile email",
             }
         )
-        try:
-            self.conn.request("POST", "/oauth/token", payload, self.headers)
-            res = self.conn.getresponse()
-            data = res.read()
-            # Parse the JSON response and save the access_token
-            response_data = json.loads(data.decode("utf-8"))
-            self.access_token = response_data.get("access_token")
-            if not self.access_token:
-                raise ValueError("Access token not found in response")
-        except Exception as e:
-            logger.error(f"Error getting auth token: {e}")
-            raise
+
+        self.conn.request("POST", "/oauth/token", payload, self.headers)
+        res = self.conn.getresponse()
+        if res.status != 200:
+            raise ValueError(f"Auth token request failed with status {res.status} and reason {res.reason}")
+        data = res.read()
+        # Parse the JSON response and save the access_token
+        response_data = json.loads(data.decode("utf-8"))
+        self.access_token = response_data.get("access_token")
+        if not self.access_token:
+            raise ValueError("Access token not found in response")
 
     def _get_user_info(self):
         payload = ""
         headers = {
             "Authorization": f"Bearer {self.access_token}",
         }
-        try:
-            self.conn.request("GET", "/userinfo", payload, headers)
-            res = self.conn.getresponse()
-            data = res.read()
-            # Parse the JSON response and return the wss_url
-            user_info = json.loads(data.decode("utf-8"))
-            self.wss_url = user_info.get("wss_url")
-            self.user_id = user_info.get("sub")
-            self.name = user_info.get("name")
-            self.user_picture = user_info.get("picture")
-            self.user_email = user_info.get("email")
-        except Exception as e:
-            logger.error(f"Error getting user info: {e}")
-            raise
+
+        self.conn.request("GET", "/userinfo", payload, headers)
+        res = self.conn.getresponse()
+        data = res.read()
+        # Parse the JSON response and return the wss_url
+        user_info = json.loads(data.decode("utf-8"))
+        self.wss_url = user_info.get("wss_url")
+        self.user_id = user_info.get("sub")
+        self.name = user_info.get("name")
+        self.user_picture = user_info.get("picture")
+        self.user_email = user_info.get("email")
